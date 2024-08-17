efbceefa4ecb05a559a3f10a481359206100e4e2
FranciscoSSequeira <113553456+FranciscoSSequeira@users.noreply.github.com>
2024-08-17 09:01:33 -0300

    Update views.sql

--- database_objects/views.sql
+++ database_objects/views.sql
@@ -9,3 +9,17 @@ FROM
     PEDIDO
 GROUP BY
     DATE (FECHA);
+
+-- view pedido por cliente
+
+USE PROYECTO_PEDIDOS;
+
+CREATE VIEW
+	PedidoPorCliente AS
+SELECT
+	IDCLIENTE AS Cliente,
+    COUNT(*) AS TotalPedidoPorCliente
+FROM
+	PEDIDO
+GROUP BY
+	IDCLIENTE;
