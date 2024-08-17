baf82c8581573ad4ab1b362087631691f86d6320
FranciscoSSequeira <113553456+FranciscoSSequeira@users.noreply.github.com>
2024-08-17 09:38:24 -0300

    Add files via upload

---
+++ data_csv/2_empleado_csv.sql
@@ -0,0 +1,13 @@
+-- insercion Empleado
+
+USE PROYECTO_PEDIDOS;
+
+INSERT INTO EMPLEADO (nombre) VALUES ('Rad');
+INSERT INTO EMPLEADO (nombre) VALUES ('Collen');
+INSERT INTO EMPLEADO (nombre) VALUES ('Salaidh');
+INSERT INTO EMPLEADO (nombre) VALUES ('Katherina');
+INSERT INTO EMPLEADO (nombre) VALUES ('Michal');
+
+SELECT
+*
+FROM proyecto_pedidos.EMPLEADO;
---
+++ data_csv/3_cliente_csv.sql
@@ -0,0 +1,28 @@
+-- insercion Cliente
+
+USE PROYECTO_PEDIDOS;
+
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ('Morgen', '170-74-3366', 'mpanton0@businessweek.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Allen', '803-70-7655', 'astickel1@salon.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Haskell', '269-85-4250', 'hlenglet2@nbcnews.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Dallis', '640-44-5858', 'dhuccaby3@addthis.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Selestina', '364-68-9879', 'sbuckell4@elpais.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Lee', '514-36-7165', 'linnis5@vistaprint.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Karee', '401-39-0619', 'kspitell6@examiner.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Rriocard', '521-70-3578', 'rfisk7@yahoo.co.jp');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Alvy', '487-75-2918', 'amahomet8@mapy.cz');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Torry', '510-18-9496', 'tlarrett9@wp.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ('Maren', '290-66-8559', 'mannakina@eventbrite.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Quint', '330-64-6817', 'qfourmyb@bizjournals.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Brunhilda', '426-34-6327', 'bkobierskic@ycombinator.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Juieta', '513-66-6885', 'jgheerhaertd@yelp.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Maris', '688-21-3405', 'mdouthwaitee@apache.org');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Ravi', '320-47-9146', 'rriggollf@upenn.edu');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Celine', '597-84-9063', 'castong@gnu.org');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Alexandro', '119-72-1755', 'aprescoteh@indiegogo.com');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ( 'Karim', '325-50-6368', 'kbrumptoni@ameblo.jp');
+INSERT INTO CLIENTE (nombre, dni, email) VALUES ('Kissee', '149-73-1895', 'kvigoursj@hud.gov');
+
+SELECT
+*
+FROM proyecto_pedidos.CLIENTE;
